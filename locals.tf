ba2add65a402f750bc9ea1144be748fd3824970a
KIRAN <ks@gmail.com>
2025-08-31 11:08:38 +0530

    NAT Gateway

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  app_private_subnets =  lookup(lookup({ for k, v in module.vpc.private_subnets : "subnets" => v.subnets }, "subnets", null), "app", null)
-  database_private_subnets =  lookup(lookup({ for k, v in module.vpc.private_subnets : "subnets" => v.subnets }, "subnets", null), "database", null)
-  frontend_private_subnets =  lookup(lookup({ for k, v in module.vpc.private_subnets : "subnets" => v.subnets }, "subnets", null), "frontend", null)
+  app_private_subnets =  lookup(lookup(lookup({ for k, v in module.vpc.private_subnets : "subnets" => v.subnets }, "subnets", null), "app", null), "subnets",null)
+  database_private_subnets =  lookup(lookup(lookup({ for k, v in module.vpc.private_subnets : "subnets" => v.subnets }, "subnets", null), "database", null), "subnets",null)
+  frontend_private_subnets =  lookup(lookup(lookup({ for k, v in module.vpc.private_subnets : "subnets" => v.subnets }, "subnets", null), "frontend", null), "subnets",null)
   }
