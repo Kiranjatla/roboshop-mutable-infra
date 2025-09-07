c5841a7dc2171e2fdc13c33d10fb88bd664a2ae0
KIRAN <ks@gmail.com>
2025-09-07 22:27:23 +0530

    ARN instance profile

--- main.tf
+++ main.tf
@@ -75,16 +75,21 @@ module "apps" {
   vpc_cidr = element([for i, j in module.vpc : j.vpc_cidr], 0)
 }
 
-module "alb" {
-  source   = "./vendor/modules/alb"
-  for_each = local.merged_alb
-  env      = var.env
-  subnets  = each.value.subnets
-  name     = each.key
-  vpc_id   = element([for i, j in module.vpc : j.vpc_id], 0)
-  vpc_cidr = element([for i, j in module.vpc : j.vpc_cidr], 0)
-  internal = each.value.internal
+#module "alb" {
+#  source   = "./vendor/modules/alb"
+#  for_each = local.merged_alb
+#  env      = var.env
+#  subnets  = each.value.subnets
+#  name     = each.key
+#  vpc_id   = element([for i, j in module.vpc : j.vpc_id], 0)
+#  vpc_cidr = element([for i, j in module.vpc : j.vpc_cidr], 0)
+#  internal = each.value.internal
+#}
+
+output ""test {
+  value = local.merged_alb
 }
+
 
 
 
